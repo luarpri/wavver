8c8fae0c8b02a83a2b9730dc7fdf6f8166dfa142
Luarpri <282300583+Luapree@users.noreply.github.com>
2026-09-26 14:18:16 +0200

    feat(encoder): encode the file directly from the script

--- encode.sh
+++ encode.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 chmod +x gba-wav-to-s3m-converter
-./gba-wav-to-s3m-converter file -i wave.wav -o audio/wave.s3m
+ffmpeg -i wave.wav -vn -c:a pcm_u8 -ar 22050 -rematrix_maxval 1.0 -ac 1 wave_converted.wav
+./gba-wav-to-s3m-converter file -i wave_converted.wav -o audio/wave.s3m
